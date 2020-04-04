cf34180617d290c0e8f412c9407e3e02bf5c0e8d
arthur6666 <qianfan@seas.upenn.edu>
2020-04-04 17:30:13 -0500

    Commit Number class

--- Sudoku/src/Number.java
+++ Sudoku/src/Number.java
@@ -1,7 +1,26 @@
-/**
- * Number
- */
+
 public class Number {
 
-    
+	private int value;
+	private boolean init;
+	
+	public Number(int value, boolean init) {
+		this.value = value;
+		this.init = init;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public boolean getInit() {
+		return init;
+	}
+	
+	public static void main(String[] args) {
+		Number nm = new Number(9, true);
+		System.out.println(nm.getValue());
+		System.out.println(nm.getInit());
+	}
 }
+
