1d34a8a62835f1d64864b1c0139d16963608b0c5
jl2154 <61812423+jl2154@users.noreply.github.com>
2020-04-05 23:34:12 -0400

    Added Timer in Secs
    created Timer in Secs.
    Suggester edited no done.

--- Sudoku/src/Sudoku.java
+++ Sudoku/src/Sudoku.java
@@ -1,7 +1,46 @@
 /**
  * Sudoku
  */
+import java.util.Date;
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
 public class Sudoku {
+    
+   /**
+    * Game StartTime
+    * @return long value
+    */
+   public long startTime() {
+       long startTime = System.currentTimeMillis();
+       return startTime;
+   }
+   /**
+    * Game Timer : End Time minus Start Time
+    * @param time
+    * @return long value 
+    */
+   public long Timer(long time) {
+       long Timer = System.currentTimeMillis()- time;
+       return Timer;
 
-    public newGame
+       
+   }
+   /**
+    * Convert timer (long value) to Seconds
+    * @param Unit
+    * @param start
+    * @return seconds passed
+    */
+   public long time(TimeUnit Unit, long start) {
+       return Unit.convert(Timer(start), TimeUnit.MILLISECONDS);
+   }
+   public static void main (String[] args) {
+       
+      Sudoku J = new Sudoku();
+      long K = J.startTime();
+      
+      System.out.println(J.startTime());
+     
+     System.out.println(J.time((TimeUnit.SECONDS),K));
+   }
 }
--- Sudoku/src/Suggester.java
+++ Sudoku/src/Suggester.java
@@ -2,6 +2,27 @@
  * Suggester
  */
 public class Suggester {
-
     
+    public Suggester() {
+        
+    }
+   
+    public void Hint(Number J){
+        
+        if(J.getOrig() == true) {
+            System.out.println(J.getValue());
+        }
+        else {
+            
+        }
+     
+        
+        
+    }
+    
+    public static void main (String[] args) {
+        Number K = new Number(1,2);
+        
+        
+    }
 }
