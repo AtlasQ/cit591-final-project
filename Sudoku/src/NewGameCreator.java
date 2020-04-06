cf7759eb490602d78fad064b957dd5f732bb4221
arthur6666 <qianfan@seas.upenn.edu>
2020-04-06 18:14:39 -0500

    Completed NumberTable class, fixed bugs in NewGameCreator class

--- Sudoku/src/NewGameCreator.java
+++ Sudoku/src/NewGameCreator.java
@@ -9,9 +9,9 @@ public class NewGameCreator {
 	// gameSeed is the random number between 1-1000 to random choose a game from the csv file
 	private int gameSeed;
 	// read number gameSeed line from puzzle.csv
-	private String puzzleC;
+	private String puzzleS;
 	// read number gameSeed line from answer.csv
-	private String answerC;
+	private String answerS;
 	
 	
 	public void setGameSeed() {
@@ -21,53 +21,62 @@ public class NewGameCreator {
 	
 
 	
-	// this will set puzzleC to the number of gameSeed line of puzzle.csv
-	public void setPuzzleC() {
+	// this will set puzzleS to the number of gameSeed line of puzzle.csv
+	public void setPuzzleS() {
 		File f = new File("puzzle.csv");
 		try {
 			Scanner sc = new Scanner(f);
 			for (int i = 0; i < gameSeed; i++) {
 				sc.next();
 			}
-			puzzleC = sc.next();
+			puzzleS = sc.next();
+			// remove double quotes
+			puzzleS = puzzleS.replaceAll("\"", "");
+			sc.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("Can't find puzzle.csv");
 		}
 	}
 	
-	// this will set puzzleC to the number of gameSeed line of answer.csv
-	public void setAnswerC() {
+	// this will set puzzleS to the number of gameSeed line of answer.csv
+	public void setAnswerS() {
 		File f = new File("answer.csv");
 		try {
 			Scanner sc = new Scanner(f);
 			for (int i = 0; i < gameSeed; i++) {
 				sc.next();
 			}
-			answerC = sc.next();
+			answerS = sc.next();
+			// remove double quotes
+			answerS = answerS.replaceAll("\"", "");
+			sc.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("Can't find answer.csv");
 		}
 	}
 	
+	
+	
 	public int getGameSeed() {
 		return gameSeed;
 	}
 	
-	public String getPuzzleC() {
-		return puzzleC;
+	public String getPuzzleS() {
+		return puzzleS;
 	}
 	
-	public String getAnswerC() {
-		return answerC;
+	public String getAnswerS() {
+		return answerS;
 	}
     
 	public static void main(String[] args) {
-		NewGameCreator nGC = new NewGameCreator();
-		nGC.setGameSeed();
-		nGC.setPuzzleC();
-		nGC.setAnswerC();
-		System.out.println(nGC.getPuzzleC());
-		System.out.println(nGC.getAnswerC());
+//		NewGameCreator nGC = new NewGameCreator();
+//		nGC.setGameSeed();
+//		nGC.setPuzzleS();
+//		nGC.setAnswerS();
+//		System.out.println(nGC.getPuzzleS());
+//		System.out.println(nGC.getPuzzleS().substring(80, 81));
+//		System.out.println(nGC.getAnswerS());
 	}
 	
 }
--- Sudoku/src/Sudoku.java
+++ Sudoku/src/Sudoku.java
@@ -3,5 +3,5 @@
  */
 public class Sudoku {
 
-    public newGame
+
 }
