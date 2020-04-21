d34da54838582b7645be77e5f402eb703e85a55d
jl2154 <61812423+jl2154@users.noreply.github.com>
2020-04-21 17:50:10 -0400

    InputReader
    Commit InputReader

--- Sudoku/src/InputReader.java
+++ Sudoku/src/InputReader.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 /**
  * InputReader
  */
 public class InputReader {
+    private int input;
 
-    
+    public int Input() {
+
+        boolean flag = false;
+        while (!flag) {
+            Scanner in = new Scanner(System.in);
+            if (in.hasNextInt()) {
+
+                input = in.nextInt();
+                flag = true;
+            } else {
+                System.out.println("Please enter a valid number!");
+
+            }
+        }
+        return input;
+    }
+
+    public static void main(String[] args) {
+        InputReader J = new InputReader();
+        J.Input();
+
+    }
 }
