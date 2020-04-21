61cb4c5b7b70f44d2e8c69e3be2cb14f903bccfa
jl2154 <61812423+jl2154@users.noreply.github.com>
2020-04-21 19:45:56 -0400

    Update Sudoku.java
    Making answer and puzzle [] [] private

--- Sudoku/src/Sudoku.java
+++ Sudoku/src/Sudoku.java
@@ -1,20 +1,30 @@
 
 public class Sudoku {
-	// call NewGameCreator class
-	NewGameCreator nGC = new NewGameCreator();
-		
-	public void newGame(){
-		// Set game seed
-		nGC.setRandomGameSeed();
-		// Read sudoku csv files
-		nGC.setPuzzleS();
-		nGC.setAnswerS();
-		String puzzleS = nGC.getPuzzleS();
-		String answerS = nGC.getAnswerS();
-		NumberTable nt = new NumberTable(puzzleS, answerS);
-		Number[][] puzzle = nt.getPuzzle();
-		Number[][] answer = nt.getAnswer();
-	}
-	
-	
+
+    // call NewGameCreator class
+    NewGameCreator nGC = new NewGameCreator();
+    private Number[][] puzzle;
+    private Number[][] answer;
+
+    public void newGame() {
+        // Set game seed
+        nGC.setRandomGameSeed();
+        // Read sudoku csv files
+        nGC.setPuzzleS();
+        nGC.setAnswerS();
+        String puzzleS = nGC.getPuzzleS();
+        String answerS = nGC.getAnswerS();
+        NumberTable nt = new NumberTable(puzzleS, answerS);
+        puzzle = nt.getPuzzle();
+        answer = nt.getAnswer();
+    }
+
+    public Number[][] getAnswer() {
+        return answer;
+    }
+
+    public Number[][] getPuzzle() {
+        return puzzle;
+    }
+
 }
