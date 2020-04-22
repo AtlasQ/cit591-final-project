994468674ef5a5444659066178b9390e4e78946c
arthur6666 <qianfan@seas.upenn.edu>
2020-04-21 21:36:40 -0500

    Created Suggester class
    This will give hints to the user. Number of hints is defined by the user

--- Sudoku/src/Suggester.java
+++ Sudoku/src/Suggester.java
@@ -2,27 +2,50 @@
  * Suggester
  */
 public class Suggester {
-    
-    public Suggester() {
-        
-    }
-   
-    public void Hint(Number J){
-        
-        if(J.getOrig() == true) {
-            System.out.println(J.getValue());
-        }
-        else {
-            
-        }
-     
-        
-        
-    }
-    
-    public static void main (String[] args) {
-        Number K = new Number(1,2);
-        
-        
-    }
+
+	int hintLeft;
+	
+	// this constructor only takes one input, how many hints do we give
+	public Suggester(int n){
+		hintLeft = n;
+	}
+
+	public void hint(int rowNum, int colNum, NumberTable nt) {
+		// hint from the answer's table
+		int ans;
+		
+		// these two numbers are the coordinate user will see
+		int userRowNum = rowNum + 1;
+		int userColNum = colNum + 1;
+		if (hintLeft >= 1) {
+			ans = nt.getAnswer()[rowNum][colNum].getValue();
+			hintLeft--;
+			System.out.println("The correct number for position (" + userRowNum + "," + userColNum + ") is " + ans + ", you have " + hintLeft + " hints left");
+		}
+
+	}
+
+	public static void main(String[] args) {
+//		NewGameCreator nGC = new NewGameCreator();
+//		nGC.setGameSeed(0);
+//		// Read sudoku csv files
+//		nGC.setPuzzleS();
+//		nGC.setAnswerS();
+//		String puzzleS = nGC.getPuzzleS();
+//		String answerS = nGC.getAnswerS();
+//		NumberTable nt = new NumberTable(puzzleS, answerS);
+//		Number[][] puzzle = nt.getPuzzle();
+//		Number[][] answer = nt.getAnswer();
+//		
+//		PrintNumberTable pNT = new PrintNumberTable();
+//		pNT.printPuzzle(nt);
+//		System.out.println();
+//		pNT.printAnswer(nt);
+//		
+//		Suggester s = new Suggester(3);
+//		s.hint(0, 0, nt);
+//		s.hint(1, 1, nt);
+//		s.hint(2, 2, nt);
+
+	}
 }
